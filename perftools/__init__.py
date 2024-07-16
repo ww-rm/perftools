fa27b30f5b65bab58ec749926d722a4b394074c2
ww-rm <43590561+ww-rm@users.noreply.github.com>
2024-07-16 16:38:08 +0800

    Update __init__.py

--- perftools/__init__.py
+++ perftools/__init__.py
@@ -4,3 +4,13 @@ PACKAGE_DATA_DIR = Path(__file__).parent.joinpath("data")
 PACKAGE_BIN_DIR = Path(__file__).parent.joinpath("bin")
 
 __version__ = "0.1.0"
+
+from .androidsdk import Sdk as _Sdk  # noqa: E402
+from .apkrepacker import ApkRepacker, do_repack  # noqa: E402
+from .simpleperf import Simpleperf  # noqa: E402
+
+# add simpleperf module path to sys.path
+_simpleperf_dir = _Sdk().get_latest_ndk().simpleperf
+if not _simpleperf_dir.is_dir():
+    raise FileNotFoundError(f"simpleperf {_simpleperf_dir} not found")
+_sys.path.append(str(_simpleperf_dir))
